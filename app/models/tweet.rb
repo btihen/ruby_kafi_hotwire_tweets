f9d00e2c768bc0d2e78fe760f732e59091c46985
Bill Tihen <btihen@gmail.com>
2021-03-26 15:33:13 +0100

    turbo broadcast updates

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
   validates :body, presence: true
+
   after_create_commit { broadcast_prepend_to "tweets" }
   after_destroy_commit { broadcast_remove_to "tweets" }
+  after_update_commit { broadcast_replace_to "tweets" }
 end
