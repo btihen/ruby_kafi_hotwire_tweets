134ab9947962df28d3cdecb157c46dcb6ac2abd3
Bill Tihen <btihen@gmail.com>
2021-03-26 14:52:46 +0100

    updates to broadcast deletes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
   validates :body, presence: true
   after_create_commit { broadcast_prepend_to "tweets" }
+  after_destroy_commit { broadcast_remove_to "tweets" }
 end
