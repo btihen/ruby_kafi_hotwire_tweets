23ff7817358cd0ca7e1ca8ce02313a6b3ca41a87
Bill Tihen <btihen@gmail.com>
2021-04-05 12:46:02 +0200

    enable update flash messages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -44,6 +44,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
+        format.turbo_stream
         format.html { redirect_to @tweet, notice: "Tweet was successfully updated." }
         format.json { render :show, status: :ok, location: @tweet }
       else
