78e67f35f346dee7ff5fc04a40e6d1296ecd3a01
Bill Tihen <btihen@gmail.com>
2021-04-11 11:22:00 +0200

    Adjustments to use JS to clear the form instead of the turbo_template

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "stimulus"
 
+// reset clears the selected element -- tie to an element with:
+// data: {controller: "reset-form", action: "turbo:submit-end->reset-form#reset"}
 export default class extends Controller {
   reset() {
     this.element.reset()
